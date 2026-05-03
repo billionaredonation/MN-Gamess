b510769e4e6a87aedb9ca8c97d60ca3c3abd00cb
billionaredonation <5fermatop@gmail.com>
2026-05-03 12:48:07 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// src/supabaseClient.js  (стр. 3-11)
 const SUPABASE_URL      = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
