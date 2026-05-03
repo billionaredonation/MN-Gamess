f0ce412546249295293f369b904a2e4b37699423
billionaredonation <5fermatop@gmail.com>
2026-05-03 03:09:44 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-  { auth: { persistSession: false } }
-);
+const SUPABASE_URL = 'https://xqgopvdqcvqikvnbxxyc.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_iAAN9KsQ4Z-qAsoNtXEYAw_9Uh5VV8o
+';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: false
+  }
+});
